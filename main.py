9555d03089ce6e6a55a6439d0e2576eb7922f297
lucasfuelling <lucasfuelling@gmail.com>
2021-07-07 16:23:15 +0800

    migrate to mariaDB

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ def line_notify_message(token, msg):
         "Authorization": "Bearer " + token,
         "Content-Type": "application/x-www-form-urlencoded"
     }
-
     payload = {'message': msg}
     r = requests.post("https://notify-api.line.me/api/notify", headers=headers, params=payload)
     return r.status_code
